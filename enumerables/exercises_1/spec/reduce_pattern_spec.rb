e38d76c2d04ff1c6b41f7adef74f09c682ed25d9
Thomas Hawley <thawley22@turing.edu>
2023-01-17 14:43:23 -0700

    Complete reduce spec, that was hard!

--- enumerables/exercises_1/spec/reduce_pattern_spec.rb
+++ enumerables/exercises_1/spec/reduce_pattern_spec.rb
@@ -8,47 +8,55 @@ RSpec.describe 'reduce pattern' do
     expect(sum).to eq(473)
   end
 
-  xit 'subtracts a list of numbers' do
+  it 'subtracts a list of numbers' do
     numbers = [28, 12, 38, 1, 91]
     difference = 0
     numbers.each do |number|
-      # Your code goes here
+      difference -= number
     end
     expect(difference).to eq(-170)
   end
 
-  xit 'multiplies a list of numbers' do
+  it 'multiplies a list of numbers' do
     numbers = [2, 3, 5, 7]
     product = 1
-    # Your code goes here
+    numbers.each {|num|
+    product *= num}
     expect(product).to eq(210)
   end
 
-  xit 'capitalizes key words in phrase' do
+  it 'capitalizes key words in phrase' do
     keywords = ["fish", "blue"]
     phrase = 'one fish two fish red fish blue fish'
-    # Your code goes here
+    keywords.each {|word| 
+    phrase.gsub!(word, word.upcase)
+  }
+
     expect(phrase).to eq('one FISH two FISH red FISH BLUE FISH')
   end
 
-  xit 'divide 560 by a bunch of numbers' do
+  it 'divide 560 by a bunch of numbers' do
     numbers = [2, 2, 2, 5, 7]
     quotient = 560
-    # Your code goes here
+    numbers.each {|num|
+      quotient /= num}
     expect(quotient).to eq(2)
   end
 
-  xit 'subtracts smallest numbers from 100' do
+  it 'subtracts smallest numbers from 100' do
     elements = [[8, 5, 3], [1, 9, 11], [4, 7, 2], [19, 34, 6]]
     difference = 100
-    # Your code goes here
+    elements.each {|element|
+      difference -= element.min}
     expect(difference).to eq(88)
   end
 
-  xit 'adds all second values together' do
+  it 'adds all second values together' do
     elements = [["a", 1], ["b", 9], ["c", 21]]
     sum = 0
-    # Your code goes here
+    elements.each {|elm|
+      sum += elm[1]
+  }
     expect(sum).to eq(31)
   end
 
--- enumerables/exercises_1/spec/reduce_spec.rb
+++ enumerables/exercises_1/spec/reduce_spec.rb
@@ -8,46 +8,45 @@ RSpec.describe 'reduce' do
     expect(result).to eq(473)
   end
 
-  xit 'subtracts a list of numbers' do
+  it 'subtracts a list of numbers' do
     numbers = [28, 12, 38, 1, 91]
     result = numbers.reduce(0) do |difference, number|
-      # Your code goes here
+      difference -= number
     end
     expect(result).to eq(-170)
   end
 
-  xit 'multiplies a list of numbers' do
+  it 'multiplies a list of numbers' do
     numbers = [2, 3, 5, 7]
-    # initial value is 1
-    # Your code goes here
+    result = numbers.reduce {|multiply, num| multiply *= num}
     expect(result).to eq(210)
   end
-
-  xit 'capitalize key words in phrase' do
+# come back to this one
+  it 'capitalize key words in phrase' do
     keywords = ["fish", "blue"]
-    # initial value is 'one fish two fish red fish blue fish'
-    # Your code goes here
+    value = 'one fish two fish red fish blue fish'
+    result = keywords.reduce(value) {|words, word| words.gsub!(word, word.upcase)}
     expect(result).to eq('one FISH two FISH red FISH BLUE FISH')
   end
 
-  xit 'divides 560 by a bunch of numbers' do
+  it 'divides 560 by a bunch of numbers' do
     numbers = [2, 2, 2, 5, 7]
-    # initial value is 560
-    # Your code goes here
+    value = 560
+    result = numbers.reduce(value) {|total, num| total /= num
+}
     expect(result).to eq(2)
   end
 
-  xit 'subtract smallest values from 100' do
+  it 'subtract smallest values from 100' do
     elements = [[8, 5, 3], [1, 9, 11], [4, 7, 2], [19, 34, 6]]
-    # initial value is 100
-    # Your code goes here
+    value = 100
+    result = elements.reduce(value) {|total, num| total -= num.min}
     expect(result).to eq(88)
   end
 
-  xit 'adds all second values together' do
+  it 'adds all second values together' do
     elements = [["a", 1], ["b", 9], ["c", 21]]
-    # initial value is 0
-    # Your code goes here
+    result = elements.reduce(0) {|sum, num| sum += num[1]}
     expect(result).to eq(31)
   end
 end
