a3b79169f123c196aaf33a3b7d239c712d8e5292
Thomas Hawley <thawley22@turing.edu>
2023-01-17 20:16:33 -0700

    Complete one spec

--- enumerables/exercises_1/spec/one_spec.rb
+++ enumerables/exercises_1/spec/one_spec.rb
@@ -8,35 +8,35 @@ RSpec.describe 'one' do
     expect(exactly_one).to eq(true)
   end
 
-  xit 'not even one ring' do
+  it 'not even one ring' do
     ornaments = ["bracelet", "anklet", "earring"]
     exactly_one_ring = ornaments.one? do |ornament|
-      # Your code goes here
+      ornament == 'ring'
     end
     expect(exactly_one_ring).to eq(false)
   end
 
-  xit 'not just one ring' do
+  it 'not just one ring' do
     ornaments = ["bracelet", "ring", "ring", "anklet", "earring"]
-    # Your code goes here
+    exactly_one_ring = ornaments.one? {|ring| ring == 'ring'}
     expect(exactly_one_ring).to eq(false)
   end
 
-  xit 'one time' do
+  it 'one time' do
     words = ["morning", "time", "evening", "noon", "dusk", "dawn"]
-    # Your code goes here
+    exactly_one_time = words.one? {|time| time == 'time'}
     expect(exactly_one_time).to eq(true)
   end
 
-  xit 'one double digit number' do
+  it 'one double digit number' do
     numbers = [8, 2, 10, 333, 9, 101]
-    # Your code goes here
+    exactly_one_double_digit = numbers.one? {|num| num > 9 && num < 100}
     expect(exactly_one_double_digit).to eq(true)
   end
 
-  xit 'not even one number' do
+  it 'not even one number' do
     numbers = [3, 20, 81, 10, 391, 32]
-    # Your code goes here
+    exactly_one_even_number = numbers.one? {|num| num.even?}
     expect(exactly_one_even_number).to eq(false)
   end
 end
