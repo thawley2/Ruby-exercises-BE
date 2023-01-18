fd4cef37993208f5746adbeb22f5d2e3d59a113c
Thomas Hawley <thawley22@turing.edu>
2023-01-17 18:19:21 -0700

    Complete all spec

--- enumerables/exercises_1/spec/all_pattern_spec.rb
+++ enumerables/exercises_1/spec/all_pattern_spec.rb
@@ -8,67 +8,85 @@ RSpec.describe 'All Pattern' do
     expect(all_zeros).to eq(true)
   end
 
-  xit 'not all zeros' do
+  it 'not all zeros' do
     numbers = [0, 0, 0, 0, 1, 0, 0, 0]
     all_zeros = true
     numbers.each do |number|
-      # Your code goes here
+      all_zeros = false if number.zero?
     end
     expect(all_zeros).to eq(false)
   end
 
-  xit 'all gone' do
+  it 'all gone' do
     words = ["gone", "gone", "gone", "gone", "gone", "gone", "gone"]
     all_gone = true
-    # Your code goes here
+    words.each {|word|
+      all_gone = false unless word.include?('gone')}
     expect(all_gone).to eq(true)
   end
 
-  xit 'not all gone' do
+  it 'not all gone' do
     words = ["gone", "gone", "gone", "gone", "gone", "there", "gone", "gone"]
-    # Your code goes here
+    all_gone = true
+    words.each {|word|
+      all_gone = false unless word.include?('gone')}
     expect(all_gone).to eq(false)
   end
 
-  xit 'all empty' do
+  it 'all empty' do
     strings = ["", "", "", "", "", "", ""]
-    # Your code goes here
+    all_empty = false
+    strings.each {|string|
+      all_empty = true if string.empty?}
     expect(all_empty).to eq(true)
   end
 
-  xit 'not all empty' do
+  it 'not all empty' do
     strings = ["", "", "", "full", "", "", ""]
-    # Your code goes here
+    all_empty = true
+    strings.each {|string|
+      all_empty = false unless string.empty?}
     expect(all_empty).to eq(false)
   end
 
-  xit 'not all uppercase' do
+  it 'not all uppercase' do
     words = ["DOUGHNUT", "CASH", "MAIN", "bOWl", "SMACK", "SAND"]
-    # Your code goes here
+    all_caps = true
+    words.each {|word|
+      all_caps = false unless word == word.upcase
+    }
     expect(all_caps).to eq(false)
   end
 
-  xit 'all lies' do
+  it 'all lies' do
     lies = [false, false, false, false]
-    # Your code goes here
+    all_lies = false
+    lies.each {|lie|
+      all_lies = true if lie == false}
     expect(all_lies).to eq(true)
   end
 
-  xit 'all multiples of seven' do
+  it 'all multiples of seven' do
     numbers = [42, 14, 35, 49, 28, 56, 21, 7]
-    # Your code goes here
+    all_multiples_of_7 = true
+    numbers.each {|num|
+      all_multiples_of_7 = false if num % 7 != 0}
     expect(all_multiples_of_7).to eq(true)
   end
 
-  xit 'not all 3 digits long' do
+  it 'not all 3 digits long' do
     numbers = [981, 831, 509, 332, 892, 8999, 110]
-    # Your code goes here
+    all_3_digits = true
+    numbers.each {|num|
+      all_3_digits = false if num > 1000}
     expect(all_3_digits).to eq(false)
   end
 
-  xit 'all four letter words' do
+  it 'all four letter words' do
     words = ["love", "hate", "fire", "bird", "call"]
-    # Your code goes here
+    all_4_letters = true
+    words.each {|word|
+      all_4_letters = false if word.length >4}
     expect(all_4_letters).to eq(true)
   end
 end
