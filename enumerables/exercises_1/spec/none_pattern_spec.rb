7468c64f0260b5397b41dcf9ad40a8d84927f6bb
Thomas Hawley <thawley22@turing.edu>
2023-01-17 18:33:35 -0700

    Complete none spec

--- enumerables/exercises_1/spec/none_pattern_spec.rb
+++ enumerables/exercises_1/spec/none_pattern_spec.rb
@@ -9,37 +9,43 @@ RSpec.describe 'none pattern' do
     expect(none_broken).to eq(true)
   end
 
-  xit 'double negative' do
+  it 'double negative' do
     numbers = [9, 3, 3, 7, 6, -5, 1]
     not_none_negative = true
     numbers.each do |number|
-      # Your code goes here
+      not_none_negative = false if number < 0
     end
     expect(not_none_negative).to eq(false)
   end
 
-  xit 'none are negative' do
+  it 'none are negative' do
     numbers = [9, 3, 1, 8, 3, 3, 5]
     none_negative = true
-    # Your code goes here
+    numbers.each {|num|
+      none_negative = false if num < 0}
     expect(none_negative).to eq(true)
   end
 
-  xit 'none shall pass' do
+  it 'none shall pass' do
     critters = ["elf", "hobbit", "dwarf", "wizard", "human"]
-    # Your code goes here
+    none_shall_pass = true
+    critters.each {|crit|
+      none_shall_pass = false if crit == 'gandolf'}
     expect(none_shall_pass).to eq(true)
   end
 
-  xit 'one or more shall pass' do
+  it 'one or more shall pass' do
     phrases = ["go", "left", "can cross", "shall pass", "must stay", "tarried"]
-    # Your code goes here
+    none_shall_pass = true
+    phrases.each {|phrase|
+      none_shall_pass = false if phrase == 'shall pass'}
     expect(none_shall_pass).to eq(false)
   end
 
-  xit 'none even' do
+  it 'none even' do
     numbers = [3, 9, 15, 21, 19]
-    # Your code goes here
+    none_even = true
+    numbers.each {|num| none_even = false if num.even?}
     expect(none_even).to eq(true)
   end
 end
