362cb239aaff04cfda93a8e49aff5e8bd7f00494
Thomas Hawley <thawley22@turing.edu>
2023-01-20 10:08:48 -0700

    Complete jackalope spec in initialize

---
+++ initialize/lib/jackalope.rb
@@ -0,0 +1,12 @@
+class Jackalope
+    def initialize(name)
+        @name = name
+        @etymology = ["Jackrabbit", "Antelope"]
+    end
+    def name
+        @name
+    end
+    def etymology
+        @etymology
+    end
+end
