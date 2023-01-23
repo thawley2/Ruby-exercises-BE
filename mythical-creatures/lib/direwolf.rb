7ea712217429c7714e2fc9b055834fd99224f3ce
Thomas Hawley <thawley22@turing.edu>
2023-01-23 15:57:37 -0700

    Complete Direwolf in MC

---
+++ mythical-creatures/lib/direwolf.rb
@@ -0,0 +1,49 @@
+class Direwolf
+    attr_reader :name, :home, :size, :starks_to_protect
+    def initialize(name, home = 'Beyond the Wall', size = 'Massive')
+        @name = name
+        @home = home
+        @size = size
+        @starks_to_protect = []
+        @hunts_white_walkers = true
+    end
+    def protects(stark)
+        if @starks_to_protect.length >= 2
+            "#{@name} can only protect 2 Starks at the same time"
+        else 
+            @starks_to_protect << stark if @home == stark.location
+            stark.safe = true
+            @hunts_white_walkers = false
+        end
+    end
+        def hunts_white_walkers?
+            @hunts_white_walkers
+        end
+        def leaves(stark)
+            @starks_to_protect = @starks_to_protect.reject {|person| person.name == stark.name}
+            stark.safe = false
+            stark
+        end
+        
+end
+
+class Stark
+    attr_reader :name, :location, :safe
+    attr_writer :safe
+    def initialize(name, location = 'Winterfell')
+        @name = name
+        @location = location 
+        @safe = false
+        
+    end
+    def safe?
+        if !@safe
+            @safe
+        else @safe
+        end
+
+    end
+    def house_words
+        'Winter is Coming'
+    end
+end
--- mythical-creatures/spec/direwolf_spec.rb
+++ mythical-creatures/spec/direwolf_spec.rb
@@ -1,5 +1,5 @@
-require './spec/spec_helper'
-require './lib/direwolf'
+require 'rspec'
+require '../lib/direwolf'
 
 RSpec.describe Direwolf do
   it 'has a name' do
@@ -48,7 +48,6 @@ RSpec.describe Direwolf do
   it 'protects the Stark children' do
     wolf = Direwolf.new('Nymeria', 'Riverlands')
     stark = Stark.new('Arya', 'Riverlands')
-
     wolf.protects(stark)
 
     expect(wolf.starks_to_protect.first.name).to eq('Arya')
@@ -79,7 +78,7 @@ RSpec.describe Direwolf do
     lady_wolf.protects(arya_stark)
 
     expect(summer_wolf.starks_to_protect).to include(sansa_stark)
-    expect(summer_wolf.starks_to_protect).to include(jon_stark)
+    expect(summer_wolf.starks_to_protect).to include(john_stark)
     expect(lady_wolf.starks_to_protect).to include(rob_stark)
     expect(lady_wolf.starks_to_protect).to include(bran_stark)
     expect(lady_wolf.starks_to_protect).to_not include(arya_stark)
@@ -129,7 +128,7 @@ RSpec.describe Direwolf do
     summer_wolf.leaves(arya_stark)
 
     expect(summer_wolf.starks_to_protect).to be_empty
-    expect(lady_wolf.starks_to_protect.first.name).to be('Sansa')
+    expect(lady_wolf.starks_to_protect.first.name).to eq('Sansa')
     expect(arya_stark.safe?).to be false
   end
 
