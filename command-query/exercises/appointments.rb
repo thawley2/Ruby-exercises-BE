4c87c6fa636f6964d981110c851a1421ad03c0fa
Thomas Hawley <thawley22@turing.edu>
2023-01-23 17:01:39 -0700

    Complte appointments in CQ

--- command-query/exercises/appointments.rb
+++ command-query/exercises/appointments.rb
@@ -1,15 +1,23 @@
 class Appointments
     def initialize
-        
+        @appointments = []
+    end
+    def at(time_slot)
+        @appointments << time_slot
+    end
+    def earliest
+        @appointments.min
+    end
 end
 
 
 
-class Time < Appointments
-    def initialize(year, month, day, hour, min= 00)
-        @year = year
-        @month = month 
-        @day = day 
-        @hour = hour
-        @min = min 
-    end
+# class Time
+#     def initialize(year, month, day, hour, min = 00)
+#         @year = year
+#         @month = month 
+#         @day = day 
+#         @hour = hour
+#         @min = min 
+#     end
+# end
